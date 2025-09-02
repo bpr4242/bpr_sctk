56d728df077f1fc2ae56e222284e002a20cc3d24
Brett Ransegnola <67807850+bpr4242@users.noreply.github.com>
2025-09-02 18:50:45 -0400

    Create __init__.py for bpr_sctoolkit module

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+bpr_sctoolkit
+===========
+
+Multimodal single-cell analysis toolkit:
+- Outlier detection
+- Scrublet wrappers
+- UMAP + Leiden grid search
+- ADT normalization + GMM denoising
+- QC visualization
+"""
+
+from .bpr_sctoolkit import *   # re-export all functions in toolkit.py
