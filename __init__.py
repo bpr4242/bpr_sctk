51f8e6b1621fc68844b0e926be8f684b8d1df736
Brett Ransegnola <67807850+bpr4242@users.noreply.github.com>
2025-09-05 14:15:05 -0400

    Clean up __init__.py by removing comments
    Removed documentation and re-export statement from __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,1 @@
-"""
-bpr_sctk
-===========
 
-Multimodal single-cell analysis toolkit:
-- Outlier detection
-- Scrublet wrappers
-- UMAP + Leiden grid search
-- ADT normalization + GMM denoising
-- QC visualization
-"""
-
-from .bpr_sctk import *   # re-export all functions in toolkit.py
